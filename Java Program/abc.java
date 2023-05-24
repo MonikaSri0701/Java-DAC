e506befcfd6ba41e8098de211e70a8b32138a845
MonikaSri0701 <128149159+MonikaSri0701@users.noreply.github.com>
2023-05-25 00:44:14 +0530

    Delete Java Program directory

--- Java Program/abc.java
+++
@@ -1,5 +0,0 @@
-class Demo {
-	public static void main(String arr[]) {
-		System.out.println("Hi world");
-	}
-}
--- Java Program/abc1.java
+++
@@ -1,5 +0,0 @@
-class Demo {
-public static void main (String Args[]){
-System.out.println("welcome");
-}
-}
