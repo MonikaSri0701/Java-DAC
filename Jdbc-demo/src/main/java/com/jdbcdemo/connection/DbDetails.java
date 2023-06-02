19c669537805fea0bd8408d5f75ab878c7161c62
MonikaSri0701 <128149159+MonikaSri0701@users.noreply.github.com>
2023-06-02 17:22:45 +0530

    Delete Jdbc-demo directory

--- Jdbc-demo/src/main/java/com/jdbcdemo/connection/DbDetails.java
+++
@@ -1,9 +0,0 @@
-package com.jdbcdemo.connection;
-
-public interface DbDetails {
-
-	String DBDRIVER = "com.mysql.cj.jdbc.Driver";
-	String CONSTR = "jdbc:mysql://localhost:3306/cdac_adv_23?useSSL=false";
-	String USERNAME = "root";
-	String PASSWORD = "Singhjay@355";
-}
